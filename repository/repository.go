ca28512d744935a66df19a02a3cd7d11985cdaeb
Jimmy <zinuhe@outlook.com>
2021-07-29 00:03:13 -0400

    Adding code

--- repository/repository.go
+++ repository/repository.go
@@ -1,1 +1,21 @@
 package repository
+
+//contain an interface for the implementation and struct for the data model.
+
+// Repository represent the repositories
+type Repository interface {
+	Close()
+	FindByID(id string) (*UserModel, error)
+	Find() ([]*UserModel, error)
+	Create(user *UserModel) error
+	Update(user *UserModel) error
+	Delete(id string) error
+}
+
+// UserModel represent the user model
+type UserModel struct {
+	Id    string
+	Name  string
+	Email string
+	Phone string
+}
